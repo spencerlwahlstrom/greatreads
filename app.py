307185fc9eaf7871b825acd17e2a64b603120fc5
Zachary Meyers <meyersz@oregonstate.edu>
2022-05-02 15:08:59 -0700

    add more books to table

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def books():
     # temporary books array
     # TODO: populate data from database
     books = []
+
     books.append({
         "book_id": 1,
         "title": "The Fellowship of the Ring",
@@ -42,6 +43,40 @@ def books():
         "msrp": 10.95,
         "average_rating": 4.5
     })
+
+    books.append({
+        "book_id": 2,
+        "title": "Good Omens: The Nice and...",
+        "publisher": "Workman",
+        "isbn": 9780060853983,
+        "summary": "According to The Nice and...",
+        "published_date": "1990-05-10",
+        "msrp": 8.99,
+        "average_rating": 4.7
+    })
+
+    books.append({
+        "book_id": 3,
+        "title": "Between the Lines",
+        "publisher": "Simon Pulse",
+        "isbn": 9781451635812,
+        "summary": "Delilah, a 15-year-old...",
+        "published_date": "2012-06-12",
+        "msrp": 8.07,
+        "average_rating": 4.3
+    })
+
+    books.append({
+        "book_id": 4,
+        "title": "Harry Potter and the Philosopher's Stone",
+        "publisher": "Bloomsbury",
+        "isbn": 9780939173341,
+        "summary": "Adaptation of the first...",
+        "published_date": "1997-06-26",
+        "msrp": 7.50,
+        "average_rating": 4.8
+    })
+
     return render_template("books.html", books=books)
 
 @app.route("/reviews")
