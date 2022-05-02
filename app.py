62b13ebfa0b6e936f1309e94ecc086cb456acd7f
Zachary Meyers <meyersz@oregonstate.edu>
2022-05-02 10:47:54 -0700

    add basic app, templates, requirements

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, json, redirect, request
+from flask_mysqldb import MySQL
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Load .env and get secrets
+load_dotenv(find_dotenv())
+
+host = os.environ.get("340DBHOST")
+user = os.environ.get("340DBUSER")
+pw = os.environ.get("340DBPW")
+db = os.environ.get("340DB")
+
+app = Flask(__name__)
+
+# Assign secrets to app config
+app.config["MYSQL_HOST"] = host
+app.config["MYSQL_USER"] = user
+app.config["MYSQL_PASSWORD"] = pw
+app.config["MYSQL_DB"] = db
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+
+# Routes
+@app.route("/books")
+def books():
+    return render_template("books.j2")
+
+@app.route("/reviews")
+def reviews():
+    return render_template("reviews.j2")
+
+# Listener
+if __name__ == "__main__":
+    app.run(port=8337, debug=True)
